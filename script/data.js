2df5633c482e4fff15163095dffb77ada6a0f2ec
Chrystian Rodolfo <87505223+kenzie-chrystian@users.noreply.github.com>
2023-10-20 13:10:44 -0300

    Update data.js

--- script/data.js
+++ script/data.js
@@ -1,81 +1,81 @@
 const data = [
   {
-    name: 'Hotel Cancun',
-    country: 'México',
+    name: "Hotel Cancun",
+    country: "México",
     price: 250,
     isOpen: true,
     toBook: [
       {
-        date: '24/05/2023',
-        isAvaliable: true,
+        date: "24/05/2023",
+        isAvailable: true,
       },
       {
-        date: '30/07/2023',
-        isAvaliable: false,
+        date: "30/07/2023",
+        isAvailable: false,
       },
       {
-        date: '04/01/2024',
-        isAvaliable: true,
+        date: "04/01/2024",
+        isAvailable: true,
       },
     ],
   },
   {
-    name: 'Pousada do Zé',
-    country: 'Brasil',
+    name: "Pousada do Zé",
+    country: "Brasil",
     price: 130,
     isOpen: true,
     toBook: [
       {
-        date: '17/03/2023',
-        isAvaliable: false,
+        date: "17/03/2023",
+        isAvailable: false,
       },
       {
-        date: '10/05/2023',
-        isAvaliable: false,
+        date: "10/05/2023",
+        isAvailable: false,
       },
       {
-        date: '21/06/2024',
-        isAvaliable: false,
+        date: "21/06/2024",
+        isAvailable: false,
       },
     ],
   },
   {
-    name: 'Copacabana Palace',
-    country: 'Brasil',
+    name: "Copacabana Palace",
+    country: "Brasil",
     price: 350,
     isOpen: false,
     toBook: [
       {
-        date: '15/01/2023',
-        isAvaliable: true,
+        date: "15/01/2023",
+        isAvailable: true,
       },
       {
-        date: '04/02/2023',
-        isAvaliable: true,
+        date: "04/02/2023",
+        isAvailable: true,
       },
       {
-        date: '09/08/2024',
-        isAvaliable: false,
+        date: "09/08/2024",
+        isAvailable: false,
       },
     ],
   },
   {
-    name: 'Empire Hotel',
-    country: 'Estados Unidos',
+    name: "Empire Hotel",
+    country: "Estados Unidos",
     price: 400,
     isOpen: false,
     toBook: [
       {
-        date: '31/12/2022',
-        isAvaliable: false,
+        date: "31/12/2022",
+        isAvailable: false,
       },
       {
-        date: '04/02/2023',
-        isAvaliable: false,
+        date: "04/02/2023",
+        isAvailable: false,
       },
       {
-        date: '29/07/2024',
-        isAvaliable: false,
+        date: "29/07/2024",
+        isAvailable: false,
       },
     ],
   },
