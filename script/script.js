968d78c814bf990a280915ca86f4abda2bdfa3a9
Albert Augusto <augustothelema@gmail.com>
2024-08-03 20:09:35 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -2,30 +2,46 @@
 
 //1) Crie uma função para filtrar os hotéis que estão localizados no Brasil
 function filterCountry(array) {
-  return;
+  let x = array.filter(e => {
+    return e.country == "Brasil";
+  })
+  return x;
 }
 console.log(filterCountry(data));
 
 // 2) Crie uma função para filtrar os hotéis que possuem o preço igual ou maior que R$200,00.
 function filterPrice(array) {
-  return;
+  let x = array.filter(e => {
+    return e.price >= 200;
+  })
+  return x;
 }
 console.log(filterPrice(data));
 
 //3) Crie uma função para filtrar os hotéis que estão abertos nesse momento (isOpen)
 function filterIsOpen(array) {
-  return;
+  let x = array.filter(e => {
+    return e.isOpen == true;
+  })
+  return x;
 }
 console.log(filterIsOpen(data));
 
 //DESAFIO
 //4) Crie uma função que filtre apenas o hotel com o nome “Copacabana Palace”. Após isso, crie outra função para verificar as datas disponíveis para agendamento nesse hotel (isAvaliable).
 function filterHotelName(array) {
-  return;
+  let x = array.filter(e => {
+    return e.name == "Copacabana Palace";
+  })
+  return x;
 }
-
 function filterToBook() {
   const hotel = filterHotelName(data);
-  return;
+  const toBook = hotel[0].toBook;
+  let disponivel = toBook.filter(e => {
+    return e.isAvailable == true;
+  })
+  
+  return disponivel;
 }
 console.log(filterToBook());
